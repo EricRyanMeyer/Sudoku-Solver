21c0272020528b134d6454dcee12f2b985ad8b2d
EricRyanMeyer <emf7301@gmail.com>
2013-10-24 23:57:54 -0500

    Implemented checkForNakedTuple to handle solving pairs, triples, etc. Made modifications for changing SudokuSet to set extension.

--- src/sudokusolver/SudokuStrategies.java
+++ src/sudokusolver/SudokuStrategies.java
@@ -5,7 +5,9 @@
 package sudokusolver;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -20,15 +22,16 @@ public class SudokuStrategies {
 	}
 
 	void solve() {
-//		SudokuCell[][] cells = grid.getGridCells();
 		boolean cellAction;
 		System.out.println("Attempting to solve...");
 
 		//Loop through solving strategies as long as one new cell is filled
 		do {
 			trimCandidates();
+//			printCandidates();
 			cellAction = solveNakedSingles();
-			cellAction = cellAction || solveNakedPairs();
+			cellAction = solveNakedPairs() || cellAction;
+			cellAction = solveNakedTriples() || cellAction;
 		} while (cellAction);
 
 //		printCandidates();
@@ -41,66 +44,110 @@ public class SudokuStrategies {
 
 	}
 
-	private boolean solveNakedPairs() {
-		System.out.println("Entering solveNakedPairs");
-//		SudokuCell[][] cells = grid.getGridCells();
-		SudokuSet[] rows = grid.getsRows();
-		SudokuSet[] columns = grid.getsColumns();
-		SudokuSet[] regions = grid.getsRegions();
-		boolean action = false;
+	private boolean solveNakedTriples() {
+		System.out.println("Entering solveNakedTriples");
+		SudokuSet[] rows = grid.getRowSetArray();
+		SudokuSet[] columns = grid.getColumnSetArray();
+		SudokuSet[] regions = grid.getRegionSetArray();
 
 		for (int i = 0; i < 9; i++) {
-			if (checkForNakedPairs(rows[i])) {
-				System.out.println("Naked pair found in row " + i);
-//				action = true;
+			Set<Integer> trim;
+
+			trim = checkForNakedTuple(rows[i], new SudokuSet(), 3);
+			if (trim != null) {
+//				System.out.println("Naked triple found in row " + i);
+//				printTrimSet(trim);
+				trimSetCandidates(rows[i], trim);
 			}
-			if (checkForNakedPairs(columns[i])) {
-				System.out.println("Naked pair found in column " + i);
-//				action = true;
+
+			trim = checkForNakedTuple(columns[i], new SudokuSet(), 3);
+			if (trim != null) {
+//				System.out.println("Naked triple found in column " + i);
+//				printTrimSet(trim);
+				trimSetCandidates(columns[i], trim);
 			}
-			if (checkForNakedPairs(regions[i])) {
-				System.out.println("Naked pair found in region " + i);
-//				action = true;
+
+			trim = checkForNakedTuple(regions[i], new SudokuSet(), 3);
+			if (trim != null) {
+//				System.out.println("Naked triple found in regions " + i);
+//				printTrimSet(trim);
+				trimSetCandidates(regions[i], trim);
 			}
 		}
-		System.out.println("Leaving solveNakedPairs");
-//		return action;
+
+		System.out.println("Leaving solveNakedTriples");
 		return solveNakedSingles();
 	}
 
-	private boolean checkForNakedPairs(SudokuSet set) {
-		List<SudokuCell> possibles, matches;
-		possibles = new ArrayList<SudokuCell>();
-		SudokuCell cellA, cellB;
+	private boolean solveNakedPairs() {
+		System.out.println("Entering solveNakedPairs");
+		SudokuSet[] rows = grid.getRowSetArray();
+		SudokuSet[] columns = grid.getColumnSetArray();
+		SudokuSet[] regions = grid.getRegionSetArray();
 
-		//Add cells with requisite candidate list size
 		for (int i = 0; i < 9; i++) {
-			cellA = set.getCell(i);
-			if (cellA.getCandidates().size() == 2) {
-				possibles.add(cellA);
+			Set<Integer> trim;
+
+			trim = checkForNakedTuple(rows[i], new SudokuSet(), 2);
+			if (trim != null) {
+//				System.out.println("Naked pair found in row " + i);
+//				printTrimSet(trim);
+				trimSetCandidates(rows[i], trim);
+			}
+
+			trim = checkForNakedTuple(columns[i], new SudokuSet(), 2);
+			if (trim != null) {
+//				System.out.println("Naked pair found in column " + i);
+//				printTrimSet(trim);
+				trimSetCandidates(columns[i], trim);
+			}
+
+			trim = checkForNakedTuple(regions[i], new SudokuSet(), 2);
+			if (trim != null) {
+//				System.out.println("Naked pair found in regions " + i);
+//				printTrimSet(trim);
+				trimSetCandidates(regions[i], trim);
 			}
 		}
 
-		if (possibles.size() >= 2) {
-			for (int i = 0; i < possibles.size(); i++) {
-				cellA = possibles.get(i);
-				for (int j = i + 1; j < possibles.size(); j++) {
-					cellB = possibles.get(j);
-					if (cellA.getCandidates().containsAll(cellB.getCandidates())) {
-						trimNakedTupleCandidates(set, cellA.getCandidates());
-						return true;
-					}
+		System.out.println("Leaving solveNakedPairs");
+		return solveNakedSingles();
+	}
+
+	private Set<Integer> checkForNakedTuple(SudokuSet source, SudokuSet match, int tupleSize) {
+		SudokuSet subsetSource = (SudokuSet) source.clone();
+		for (SudokuCell cell : source) {
+			//remove current cell from source clone to create subset for recursive call
+			subsetSource.remove(cell);
+			int numCandidates = cell.getCandidates().size();
+			if ((numCandidates > 1) && (numCandidates <= tupleSize)) {
+				//clone match and add cell to create superset for validation and recursive call
+				SudokuSet superMatch = (SudokuSet) match.clone();
+				superMatch.add(cell);
+
+				//Build set of unique candidates from cells in superMatch
+				Set<Integer> tupleSet = new LinkedHashSet<Integer>();
+				for (SudokuCell cell2 : superMatch) {
+					tupleSet.addAll(cell2.getCandidates());
+				}
+
+				if (tupleSet.size() > tupleSize) {
+					continue;
+				} else if (superMatch.size() < tupleSize) {
+					return checkForNakedTuple(subsetSource, superMatch, tupleSize);
+				} else {
+					return tupleSet;
 				}
 			}
 		}
-		return false;
+		return null;
 	}
 
 	private boolean solveNakedSingles() {
 		System.out.println("Entering solveNakedSingles");
 		SudokuCell[][] cells = grid.getGridCells();
 		boolean singleFound = false;
-		List<Integer> candidates;
+		Set<Integer> candidates;
 		for (int r = 0; r < 9; r++) {
 			for (int c = 0; c < 9; c++) {
 				//if cell is empty(==0), check possibilities and fill cell if only one exists
@@ -108,7 +155,8 @@ public class SudokuStrategies {
 					trimCellCandidates(cells[r][c]);
 					candidates = cells[r][c].getCandidates();
 					if (candidates.size() == 1) {
-						cells[r][c].setValue(candidates.get(0));
+						Integer val = candidates.iterator().next();
+						cells[r][c].setValue(val.intValue());
 						singleFound = true;
 					}
 				}
@@ -118,11 +166,11 @@ public class SudokuStrategies {
 		return singleFound;
 	}
 
-	private void trimNakedTupleCandidates(SudokuSet set, List<Integer> trim) {
-		for (int i = 0; i < 9; i++) {
-			//don't remove candidates from naked tuples
-			if (!trim.containsAll(set.getCell(i).getCandidates())) {
-				trimCellCandidates(set.getCell(i), trim);
+	private void trimSetCandidates(SudokuSet set, Set<Integer> trim) {
+		//Only trim candidates from cell if they are subset of cell candidates
+		for (SudokuCell cell : set) {
+			if (!trim.containsAll(cell.getCandidates())) {
+				trimCellCandidates(cell, trim);
 			}
 		}
 	}
@@ -142,8 +190,8 @@ public class SudokuStrategies {
 		trimCellCandidates(cell, cell.getRegionSet().getCellValues());
 	}
 
-	private void trimCellCandidates(SudokuCell cell, List<Integer> trim) {
-		List<Integer> candidates = cell.getCandidates();
+	private void trimCellCandidates(SudokuCell cell, Set<Integer> trim) {
+		Set<Integer> candidates = cell.getCandidates();
 		candidates.removeAll(trim);
 	}
 
@@ -151,20 +199,28 @@ public class SudokuStrategies {
 		SudokuCell[][] cells = grid.getGridCells();
 		for (int r = 0; r < 9; r++) {
 			for (int c = 0; c < 9; c++) {
-				List<Integer> candidates = cells[r][c].getCandidates();
+				Set<Integer> candidates = cells[r][c].getCandidates();
 				System.out.print(r + "x" + c + " candidates:");
-				for (int i = 0; i < candidates.size(); i++) {
-					System.out.print(candidates.get(i).toString() + ",");
+				for (Integer val : candidates) {
+					System.out.print(val.toString() + ",");
 				}
 				System.out.println("");
 			}
 		}
 	}
 
+	private void printTrimSet(Set set) {
+		System.out.print("Set values:");
+		for (Object val : set) {
+			System.out.print(val + ",");
+		}
+		System.out.println("");
+	}
+
 	private boolean validateGrid() {
-		SudokuSet[] rows = grid.getsRows();
-		SudokuSet[] columns = grid.getsColumns();
-		SudokuSet[] regions = grid.getsRegions();
+		SudokuSet[] rows = grid.getRowSetArray();
+		SudokuSet[] columns = grid.getColumnSetArray();
+		SudokuSet[] regions = grid.getRegionSetArray();
 		for (int i = 0; i < 9; i++) {
 			if (!validateSet(rows[i])) {
 				return false;
@@ -181,9 +237,9 @@ public class SudokuStrategies {
 
 	private boolean validateSet(SudokuSet set) {
 		List<Integer> values = new ArrayList<Integer>();
-		for (int i = 0; i < 9; i++) {
-			if (set.getCell(i).getValue() != 0) {
-				Integer x = new Integer(set.getCell(i).getValue());
+		for (SudokuCell cell : set) {
+			Integer x = new Integer(cell.getValue());
+			if (x != 0) {
 				if (values.contains(x)) {
 					return false;
 				} else {
